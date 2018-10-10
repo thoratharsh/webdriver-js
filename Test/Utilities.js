3e082c90c4e131887c6009aa6db855d02f88c28d
thoratharsh <thoratharsh@users.noreply.github.com>
2018-10-11 00:53:04 +0530

    changes to Utility

--- Test/Utilities.js
+++ Test/Utilities.js
@@ -8,18 +8,93 @@ class Utilities {
     }
 
     inputText(locator, text) {
-        browser.addValue(locator, text);
+        try {
+            browser.addValue(locator, text);
+        }
+        catch (err) {
+            console.log("Element " + locator + "is not found")
+        }
     }
-    clearText(locator, text) {
 
+    clearText(locator) {
+        try {
+            browser.clearElement(locator);
+        }
+        catch (err) {
+            console.log("Element " + locator + "is not found")
+        }
     }
+
     selectPicklistValueByIndex(locator, index) {
-        browser.selectByIndex(locator, index);
+        try {
+            browser.selectByIndex(locator, index);
+        }
+        catch (err) {
+            console.log("Element " + locator + "is not found")
+        }
     }
+
+    selectPicklistByValue(locator, value) {
+        browser.selectByValue(locator, value);
+    }
+
+    selectPicklistByVisibleText(locator, visibleText) {
+        browser.selectByVisibleText(locator, visibleText);
+    }
+
     waitUntilElementIsVisible(locator, waitTime = 10000) {
         browser.waitForVisible(locator, waitTime);
     }
 
+    get getTextOfElement(locator) {
+        return browser.element(locator).getText;
+    }
+
+    get getTitleOfCurrentPage() {
+        return browser.getTitle();
+    }
+
+    get getURLOfCurrentPage() {
+        return browser.getUrl;
+    }
+
+    acceptAlert() {
+        browser.acceptAlert();
+    }
+
+    dismissAlert() {
+        browser.dismissAlert();
+    }
+
+    get getAlertText() {
+        return browser.getAlertText();
+    }
+
+    navigateBack() {
+        browser.back();
+    }
+
+    navigateForward(){
+        browser.forward();
+    }
+
+    get getNumberofMatchingElementsBySelector(locator) {
+        var numberOfElements = browser.elements(locator);
+        return numberOfElements.length;
+    }
+
+    switchFrame(locator){
+        browser.frame(locator);
+    }
+
+    switchToDefaultFrame(){
+        browser.frameParent();
+    }
+
+    reloadPage(){
+        browser.refresh();
+    }
+
 }
 
-module.exports=new Utilities();
+module.exports = new Utilities();
